2eec4e94fd3172719142c3dfd49609b0300c983e
marioccomp <marioccomp@gmail.com>
2025-12-06 20:01:45 -0300

    adicionei scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>amo você ❤️❤️</h1>
+  return <h1>amo você ❤️❤️</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
