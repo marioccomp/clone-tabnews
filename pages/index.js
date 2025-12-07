26936c85225b1b7d026379a48bc00a6244fc4142
marioccomp <marioccomp@gmail.com>
2025-12-07 16:32:48 -0300

    install `jest` and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>amo você ❤️❤️</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
